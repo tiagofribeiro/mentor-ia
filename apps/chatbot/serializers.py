3c6678ef26fe8ef620f26b873842adaa85cf2961
Tiago Ribeiro <tg.frnds@outlook.com>
2025-06-24 17:08:06 -0300

    :gear: wip: pdf reading

--- apps/chatbot/serializers.py
+++ apps/chatbot/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
-from apps.chatbot.models import ChatMessage, ChatSession, UploadedDocument
+from apps.chatbot.models import UploadedDocument
 
 class ChatMessageSerializer(serializers.Serializer):
     message = serializers.CharField(write_only=True)
     response = serializers.CharField(read_only=True)
+
+class UploadedDocumentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UploadedDocument
+        fields = ('file', 'title')
--- core/settings/dev.py
+++ core/settings/dev.py
@@ -97,6 +97,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 REST_FRAMEWORK = {
